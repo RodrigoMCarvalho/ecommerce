113d90e2ed67f4f8c0f04f66c6a282df2c3a33f8
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-02-19 18:54:52 -0300

    criada a classe PageAdmin e tpl admin

--- index.php
+++ index.php
@@ -4,13 +4,21 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+//configurações de rotas
 $app->get('/', function() {
     $page = new Page();
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    $page = new PageAdmin();
     $page->setTpl("index");
 
 });
